ebea716c611c73b01130d0d19024b06460bc1a55
birame <birame.cisse@telecom-paris.fr>
2026-04-03 10:27:52 +0200

    TimeStop SFX debug

--- godot_mapping/godot_cube/scripts/game_scenes/rotation_cube.gd
+++ godot_mapping/godot_cube/scripts/game_scenes/rotation_cube.gd
@@ -1,18 +1,25 @@
 extends AnimatableBody3D
 
 @onready var landmarks_proceed = $"../LandMarksProceed"
-@onready var time_stop_sound: AudioStreamPlayer = $"../../TimeStopSound"
+@onready var time_stop_sound: AudioStreamPlayer = $"../TimeStopSound"
 @export var vitesse_rotation = 1.0
+@onready var test_label: Label = $"../TestLabel"
 
 var facteur_rotation = 2.0
+var last_gesture = ""
 
 """!-! TACHES FAITES A CHAQUE FRAME !-!"""
 func _physics_process(delta: float) -> void:
 	# Rotation du cube.
 	# Prise en compte des FPS de l'ordinateur.
-	if landmarks_proceed._maj_speed()[1]== "Closed_Fist" :
-		#time_stop_sound.play()
+	if landmarks_proceed._maj_speed()[1] == "Closed_Fist" :
+		if last_gesture != "Closed_Fist" :
+			last_gesture = "Closed_Fist"
+			time_stop_sound.play()
+			test_label.text = "ZA WARUDO"
 		vitesse_rotation = 0
 	else :
+		last_gesture = ""
 		vitesse_rotation = facteur_rotation*landmarks_proceed._maj_speed()[0]
+		test_label.text = ""
 	rotate_y(vitesse_rotation * delta)
