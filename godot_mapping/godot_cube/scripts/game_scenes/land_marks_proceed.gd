ab7957fccef666365364bbbd9095a8790109e8ae
NGUYEN Van-Kévin <van-kevin.nguyen@telecom-paris.fr>
2026-04-03 19:08:08 +0200

    Correction de bugs visuels sur l'affichage du texte lié à la caméra à cause du thread

--- godot_mapping/godot_cube/scripts/game_scenes/land_marks_proceed.gd
+++ godot_mapping/godot_cube/scripts/game_scenes/land_marks_proceed.gd
@@ -9,11 +9,13 @@ var time_detect
 var time_render
 var time_display
 var camera_fps
+var output_image : Image = null
+var last_hand_detected_time : float = -999.0
+var hand_display_duration : float = 0.05
 
 # Thread
 var thread_mp: Thread
 var runThread : bool = true
-
 
 var running_mode = 1 # Mode Vidéo
 var num_hands = 4
@@ -31,13 +33,12 @@ var result_mediapipe= null
 @onready var debug_view = $CanvasLayer/DebugOverlay # Un TextureRect pour voir le résultat
 @onready var cube = $".."/Cube
 @onready var label: Label = $"../CameraLabel"
+
 func _ready():
 	thread_mp=Thread.new()
 	mutex = Mutex.new()
 	thread_mp.start(_thread_mediapipe)
 	_setup_camera()
-
-
 
 func _setup_mediapipe():
 	# Récupère le modèle hand_landmarker.task et l'initialise
@@ -78,7 +79,7 @@ func _start_camera():
 	CameraServer.monitoring_feeds = true
 	var feeds = CameraServer.feeds()
 	if feeds.size() > 0:
-		camera_feed = feeds[1]
+		camera_feed = feeds[0]
 		camera_feed.feed_is_active = true
 		
 		var tex = CameraTexture.new()
@@ -131,9 +132,9 @@ func _thread_mediapipe():
 				var output = renderer.render(mp_image, result.hand_landmarks)
 				time_render = (Time.get_ticks_usec()-start_render)/1000.0
 				
-				var start_display = Time.get_ticks_usec()
-				update_debug_overlay(output.image)
-				time_display = (Time.get_ticks_usec()-start_display)/1000.0
+				mutex.lock()
+				output_image = output.image
+				mutex.unlock()
 				
 				# Envoie de result au main process
 				mutex.lock()
@@ -144,6 +145,16 @@ func _thread_mediapipe():
 func _process(_delta):
 	camera_fps = Engine.get_frames_per_second()
 	label.text = ""
+
+	# Récupération et affichage de l'image de rendu MediaPipe
+	var out_img = null
+	mutex.lock()
+	out_img = output_image
+	output_image = null
+	mutex.unlock()
+	if out_img:
+		update_debug_overlay(out_img)
+
 	# Récupération de l'image du Viewport
 	var tex = viewport.get_texture()
 	var img = tex.get_image()
@@ -162,14 +173,20 @@ func _process(_delta):
 	result_mediapipe = null
 	mutex.unlock()
 	
-	
 	# Traitement des mains détectées
-	if result :
+	if result:
 		for i in range(result.hand_landmarks.size()):
+			last_hand_detected_time = Time.get_ticks_msec() / 1000.0
 			label.text = "Main détectée"
 			var hand_landmarks = result.hand_landmarks[i]
 			modulated = hand_landmarks.landmarks[8].x
-			_maj_speed() # On passe l'index de la main (0 ou 1)
+			_maj_speed()
+	
+	# On évite de changer le texte trop vite même si on perd quelques frames à cause du thread
+	if Time.get_ticks_msec() / 1000.0 - last_hand_detected_time < hand_display_duration:
+		label.text = "Main détectée"
+	else:
+		label.text = ""
 
 func _maj_speed():
 	# Tout ce qui concerne la gestion des données relatives aux mains se fait ici
